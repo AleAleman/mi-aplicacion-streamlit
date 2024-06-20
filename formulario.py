1cecd15712df4f5e445533c5ba38ede4d730d22f
AleAleman <alejandra.aleman@bidataexplorers.com>
2024-06-20 05:51:31 -0600

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -5,19 +5,23 @@ import os
 import json
 from oauth2client.service_account import ServiceAccountCredentials
 
+import os
+import json
+from oauth2client.client import OAuth2WebServerFlow
+
 # Carga la cadena JSON desde una variable de entorno
 json_creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
 if json_creds is None:
-    st.error("La variable de entorno 'GOOGLE_APPLICATION_CREDENTIALS_JSON' no está configurada.")
-    st.stop()
+    raise ValueError("La variable de entorno 'GOOGLE_APPLICATION_CREDENTIALS_JSON' no está configurada.")
 
-try:
-    creds_dict = json.loads(json_creds)
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
-    client = gspread.authorize(creds)
-except json.JSONDecodeError:
-    st.error("Error al decodificar el JSON de las credenciales. Verifique el formato del JSON.")
-    st.stop()
+creds_dict = json.loads(json_creds)
+flow = OAuth2WebServerFlow(
+    client_id=creds_dict['web']['client_id'],
+    client_secret=creds_dict['web']['client_secret'],
+    scope=['https://www.googleapis.com/auth/drive'],
+    redirect_uri=creds_dict['web']['redirect_uris'][0]
+)
+
 
 # Abrir la hoja de cálculo de Google por nombre
 sheet = client.open("App_streamlit").sheet1
